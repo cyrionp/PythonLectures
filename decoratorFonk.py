b7589f2ca7ef47cbf112f3ec564fcc769a400d91
cyrionpow <qadrrdrmz@gmail.com>
2020-08-30 23:07:10 +0300

    Update decoratorFonk.py

--- decoratorFonk.py
+++ decoratorFonk.py
@@ -1,3 +1,59 @@
+'''
 def my_decorator(func):
-    def wrapper():
+    def wrapper(name):
         print("Fonksiyondan önceki işlemler")
+        func(name)
+        print("Fonksiyondan sonraki işlemler")
+    return wrapper
+
+def sayHello():
+    print("hello")
+
+def sayGreeting():
+    print("greeting")
+
+@my_decorator
+def sayHello(name):
+    print(f"hello {name}")
+
+sayHello("Ali")
+'''
+
+import math
+import time
+
+def calculate_time(func):
+    def inner(*args,**kwargs):
+        start=time.time()
+        time.sleep(1)
+        func(*args,**kwargs)
+        finish=time.time()
+        print(f"Fonksiyon {func.__name__} {str(finish-start)} saniye sürdü")
+    return inner
+
+@calculate_time
+def usalma(a,b):
+    print(math.pow(a,b))
+
+@calculate_time
+def faktoriyel(num):
+    print(math.factorial(num))
+
+@calculate_time
+def toplama(a,b):
+    print(a+b)
+
+@calculate_time
+def cikarma(a,b):
+    print(a-b)
+
+@calculate_time
+def carpma(a,b):
+    print(a*b)
+
+@calculate_time
+def bolme(a,b):
+    print(a/b)
+
+usalma(2,3)
+faktoriyel(4)
